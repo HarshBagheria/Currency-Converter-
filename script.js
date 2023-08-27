11f9d0358d629605ef273bb3d71ed27f19653e17
HarshBagheria <114928571+HarshBagheria@users.noreply.github.com>
2023-08-27 19:33:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("CONVERT YOUR CURRENCY EASILY");
+
+const populate = async(value, currency) => {
+    let myStr = "";
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_8FBqslFShFGDZI85nveyAfFaFBH7uzOAeRfItcN4&base_currency="+currency;
+    // fetch(url);
+    let response = await fetch(url);
+    let rJson = await response.json();
+    document.querySelector(".output").style.display="block";
+
+    for(let key of Object.keys(rJson["data"])){
+        myStr +=  ` <tr>
+                        <td>${key}</td>
+                        <td>${rJson["data"][key]["code"]}</td>
+                        <td>${Math.round(  rJson["data"][key]["value"]*value)}</td>
+                    </tr>
+                 `
+    }
+
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myStr ;
+    
+}
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    // console.log("Submit Button is clicked");
+
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+
+    const currency = document.querySelector("select[name='currency']").value;
+
+    populate(value, currency);
+
+})
